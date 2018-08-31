811b18c092d3e47da0cbdff4a6e0100ab2766545
Segger87 <Segger87@gmail.com>
2018-08-31 15:44:14 +0100

    Added top 10 trending Gifs page which uses giphy API

---
+++ scripts/giphy.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+	console.log("DOM fully loaded and parsed");
+
+	var trendingUrl = "https://api.giphy.com/v1/gifs/trending?api_key=jmJFGQx32Mt6etpKr7aTpjXmn977kSYi&limit=10&rating=G";
+	var giphyContainer = document.getElementById("giphy-container");
+
+	function writeSinglePostToPage(post) {
+
+		var containerDiv = document.createElement("div");
+		containerDiv.classList.add("giphy-posts");
+		giphyContainer.appendChild(containerDiv);
+
+		if (post.images) {
+			var newImage = document.createElement("img");
+			newImage.classList.add("giphy-image");
+			newImage.src = post.images.downsized.url;
+			containerDiv.appendChild(newImage);
+		}
+
+		giphyContainer.appendChild(containerDiv);
+	}
+
+	function writeAllPostsToPage(postArray) {
+		postArray.forEach(writeSinglePostToPage);
+	}
+
+	function parseJSONFromResponse(response) {
+		return response.json();
+	}
+
+	function extractPostArrayFromJSON(json) {
+		console.log(json.data);
+		return json.data;
+	}
+
+	function handleError(err) {
+		console.log(err);
+	}
+
+	fetch(trendingUrl)
+		.then(parseJSONFromResponse)
+		.then(extractPostArrayFromJSON)
+		.then(writeAllPostsToPage)
+		.catch(handleError);
+});
